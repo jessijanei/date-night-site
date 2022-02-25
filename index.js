ae3db2b1ea25cc1e8974bbc1f76aa5327265ed49
Shannon Powers <tmie2223@gmail.com>
2022-02-24 16:36:18 -0800

    updated css and js files

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+//function for handling submit event
+let handleSubmit = (event) => {
+    event.preventDefault(); 
+    console.log(event);
+
+//fetch??
+    fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=78d7dfa988514b18b52ac1ab9f833947&cuisine=italian").then((response) => {
+    response.json();    
+    console.log(response)
+  });
+
+}
+//adding event listener to cuisine submit button
+let submit = document.getElementById('submitBtn');
+submit.addEventListener("click", handleSubmit);
